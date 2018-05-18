9a80d7bf02ae328fe268091f3256f28ab1fd6328
hulyaserminkarakas <32978851+hulyaserminkarakas@users.noreply.github.com>
2018-05-18 18:10:20 +0300

    Main changes

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+
+
+	void Start () {
+
+
+
+	}
+
+
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0)){
+
+			Vector2 ray = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (ray, Vector2.zero);
+
+			if (hit.collider != null) {
+				Destroy (hit.transform.gameObject);
+			}
+		
+		}
+
+	}
+}
